6ff77b0e3f43f17392b894655bd1e7eaf50b3858
Amit Gupta <akaamitgupta@gmail.com>
2016-09-21 23:09:41 +0530

    Initial commit

---
+++ src/SneakerServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Squareboat\Sneaker;
+
+use Illuminate\Support\ServiceProvider;
+
+class SneakerServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // 
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // 
+    }
+}
