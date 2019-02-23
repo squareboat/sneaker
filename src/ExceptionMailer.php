46f1d9e68510643c0bb432999f71f97bda2d2057
codegain <steininger.mcl@gmail.com>
2019-02-23 22:20:36 +0100

    Update ExceptionMailer.php

--- src/ExceptionMailer.php
+++ src/ExceptionMailer.php
@@ -7,7 +7,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class ExceptionMailer extends Mailable implements ShouldQueue
+class ExceptionMailer extends Mailable
 {
     use Queueable, SerializesModels;
 
